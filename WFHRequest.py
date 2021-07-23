16569388995baf59d4ea6b263995a03b0107f5b8
Dark <darkgrouptw@gmail.com>
2021-07-23 23:43:27 +0800

    1. 優化程式 2. 下班打卡流程優化

--- WFHRequest.py
+++ WFHRequest.py
@@ -6,10 +6,16 @@ from ScreenShotManager import Manager as SSM
 import sys
 import os
 import re
+import schedule
+import time
 
 # 時間間隔多久，打卡一次
-TimeOffsetMins = 5
+TimeOffsetMins = 2
 TimeIntervalMins = 30
+
+# 運作的變數
+Cookies
+IsFinishToday = False
 
 # Debug Mode
 IsDebugMode = True
@@ -27,8 +33,25 @@ def __PrintSplitLine():
         Result += "-"
     print(Result)
 
+# 登入頁面
+def LoginProcess():
+    global Cookies
+    LoginM = FDM.Manager("http://tpehr.wkec.com/ehrportal/LoginFOrginal.asp")
+    LoginM.AddParams("op", "act")
+    LoginM.AddParams("lmethod", 3)
+    LoginM.AddParams("companyno", 47088673)
+    LoginM.AddParams("companyid", 1)
+    LoginM.AddParams("username", sys.argv[1])
+    LoginM.AddParams("password", sys.argv[2])
+    Response = LoginM.Post()
+    Cookies = Response.cookies
+
 # 上班打卡
 def PunchInProcess(Cookies):
+    # 連線
+    if not IsDebugMode:
+        VPN.Connect()
+
     CardDataM = FDM.Manager("http://tpehr.wkec.com/ehrportal/DEPT/Personal_CardData_Default.asp", Cookies)
     CardDataM.AddParams("OP", "Update")
     CardDataM.AddParams("hidCARD_TYPE", "0")
@@ -38,9 +61,17 @@ def PunchInProcess(Cookies):
         print(PunchInTime + " 上班打卡成功 !!")
     else:
         print("上班打卡失敗 !!")
+    
+    # 斷開
+    if not IsDebugMode:
+        VPN.Discount()
 
 # 下班卡
 def PunchOutProcess(Cookies):
+    # 連線
+    if not IsDebugMode:
+        VPN.Connect()
+
     CardID = __GetCardID(Cookies, 1)
     CardDataM = FDM.Manager("http://tpehr.wkec.com/ehrportal/DEPT/Personal_CardData_Default.asp", Cookies)
     CardDataM.AddParams("hidCARD_TYPE", "1")
@@ -57,7 +88,16 @@ def PunchOutProcess(Cookies):
         print(PunchOutTime + " 下班打卡成功 !!")
     else:
         print("下班打卡失敗 !!")
-    # pass
+
+    # 斷開
+    if not IsDebugMode:
+        VPN.Discount()
+        
+# 最後一次的下班卡
+def PunchOutProcess_Final(Cookies):
+    global IsFinishToday
+    PunchOutProcess(Cookies)
+    IsFinishToday = True
 
 # 拿卡片 ID
 def __GetCardID(Cookies, Type):
@@ -71,7 +111,7 @@ def __GetCardID(Cookies, Type):
 
 # 截圖
 def __ScreenShot(Response, Location):
-    print(SSM.Manager.Transfor(Response))
+    # print(SSM.Manager.Transfor(Response))
     return True
 #endregion
 
@@ -80,15 +120,7 @@ def __ScreenShot(Response, Location):
 #region
 if not IsDebugMode:
     VPN.Connect()
-LoginM = FDM.Manager("http://tpehr.wkec.com/ehrportal/LoginFOrginal.asp")
-LoginM.AddParams("op", "act")
-LoginM.AddParams("lmethod", 3)
-LoginM.AddParams("companyno", 47088673)
-LoginM.AddParams("companyid", 1)
-LoginM.AddParams("username", sys.argv[1])
-LoginM.AddParams("password", sys.argv[2])
-Response = LoginM.Post()
-Cookies = Response.cookies
+LoginProcess()
 print(Cookies.get_dict())
 
 # 今天日期
@@ -101,9 +133,9 @@ print("日期:", TodayString)
 # 接者進打卡頁面
 # 1. 拿取目前的打卡列表
 # 2. 如果沒有打上班卡，立即幫忙打上班卡
-# 3. 如果打了上班卡，接下來每隔 30 分鐘會幫忙打卡一次 (延遲五分鐘 的每隔30分)
-# 3.5 可能考慮截圖，避免有打卡但沒有成功
-# 4. 如果打卡到最後一個時候
+# 3. 如果打了上班卡，接下來每隔 30 分鐘會幫忙打卡一次 (延遲五分鐘 的每隔30分)，並且截圖
+# 4. 一直循環直到打完卡 Progress
+# 5.
 DoesWorkStart = False
 WorkStartTime = datetime.now()
 DoesWorkEnd = False
@@ -128,35 +160,66 @@ __PrintSplitLine()
 
 # 2.
 #region
-Schedule = []
+ScheduleTimeList = []
 if not DoesWorkStart:
     PunchInProcess(Cookies)
 
 print("本日上班打卡時間：")
 print(WorkStartTime)
-print()
-WorkStartTime +=  timedelta(minutes=TimeOffsetMins)
-FinalWorkTime = WorkStartTime + timedelta(hours=9.25)
+# WorkStartTime +=  timedelta(minutes=TimeOffsetMins)
+TempTime = WorkStartTime + timedelta(minutes=TimeOffsetMins)
+FinalWorkTime = TempTime + timedelta(hours=9.25)
+__PrintSplitLine()
 #endregion
 
 # 3.
 # 先把所有上班的打卡時間加入
 #region
 for i in range (int(9.25 * 60 / TimeIntervalMins)):
-    WorkStartTime += timedelta(minutes=TimeIntervalMins)
-    Schedule.append(WorkStartTime)
-Schedule.append(FinalWorkTime)
-for i in range(len(Schedule) - 1, -1, -1):
-    if Schedule[i] < Today:
-        Schedule.pop(i)
+    TempTime += timedelta(minutes=TimeIntervalMins)
+    ScheduleTimeList.append(TempTime)
+ScheduleTimeList.append(FinalWorkTime)
+for i in range(len(ScheduleTimeList) - 1, -1, -1):
+    if ScheduleTimeList[i] < Today:
+        ScheduleTimeList.pop(i)
 
 # 顯示接下來要打卡的時間
-for i in range(len(Schedule)):
-    print(Schedule[i])
-PunchOutProcess(Cookies)
+# 這裡有幾種情況
+# 1. 有 Schdule 的時間 (代表表有排行程)
+# 2. 沒打下班卡，等下班後才打開這個 程式
+# 3. 有打下班卡，但總時間差，小於 9 個小時又 15 分 + TimeOffsetMins
+# 4. 不用在打卡了
+print("接下來打卡時間")
+if len(ScheduleTimeList) > 0:
+    # 1.
+    for i in range(len(ScheduleTimeList)):
+        print("排成在 " + ScheduleTimeList[i] + " 會自動打下班卡")
+        if i != len(ScheduleTimeList) - d:
+            schedule.every().day.at(ScheduleTimeList[i]).do(PunchOutProcess(Cookies))
+        else:
+            schedule.every().day.at(ScheduleTimeList[i]).do(PunchOutProcess_Final(Cookies))
+elif not DoesWorkEnd or (DoesWorkEnd and WorkEndTime - WorkStartTime < timedelta(hours=9.25) + timedelta(minutes=TimeOffsetMins)):
+    # 2. && 3.
+    IsFinishToday = True
+    print("現在打下班卡")
+    PunchOutProcess(Cookies)
+else:
+    # 4.
+    print("本日不用打卡了")
+    IsFinishToday = True
+__PrintSplitLine()
 #endregion
 
 # 4.
-# 
+#region
 if not IsDebugMode:
     VPN.Discount()
+while not IsFinishToday:
+    schedule.run_pending()
+    time.sleep(1)
+PunchOutProcess(Cookies)
+
+#endregion
+
+# 5.
+# 
